45d10e5ff0ea7cd18a6d2e7e0e4e484b7013d351
hahriar <closebrocircle@gmail.com>
2021-08-19 21:57:24 +0600

    last comit

--- js/banking.js
+++ js/banking.js
@@ -2,7 +2,7 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
     // get the deposit money llll
 
-    
+
     const depositInput = document.getElementById('deposit-input');
     const newDepositAmount = depositInput.value;
 
@@ -19,11 +19,44 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    const newBalanceTotal = previousBalanceTotal + parseFloat(newDepositAmount);
+    const previousWithdrawTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousWithdrawTotal + parseFloat(newDepositAmount);
     balanceTotal.innerText = newBalanceTotal;
 
     // clear the diposit input field
 
     depositInput.value = '';
+});
+
+//Withdraw Ammount Even Handler llll
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+ // Withdraw total 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // Update Balance section 
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+
+    // clear withdraw input
+
+    withdrawInput.value = '';
+    
+
 });
